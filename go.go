ff6b5264f3bbd69176e916451f351587e4ab28e2
raunakspecial1994 <raunakdugar.nitdgp@gmail.com>
2015-05-15 08:39:38 +0530

    First Commit

---
+++ go.go
@@ -0,0 +1,14 @@
+package main
+import "fmt"
+func add(x, y int) int{
+return x+y
+}
+var java string = "hello"
+func main(){
+fmt.Println("Hello!!")
+A:=[5]float64{2.0,9.2,44.8,29.9,77}
+fmt.Println(A[3])
+fmt.Println(add(22,33))
+fmt.Println(java)
+}
+

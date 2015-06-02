4e6fd263b35fd196f1cdf0aa81eb1fe26f4e4e04
raunakspecial1994 <raunakdugar.nitdgp@gmail.com>
2015-06-02 15:25:35 +0530

    Update bolt.go

--- Boltdb-Aerospike/bolt.go
+++ Boltdb-Aerospike/bolt.go
@@ -4,59 +4,40 @@ import (
     "log"
     "fmt"
     "github.com/boltdb/bolt"
+    "time"
 )
 func main(){
-db, err :=bolt.Open("nit.db",0600,nil)
+db, err :=bolt.Open("dfm.db",0600,&bolt.Options{Timeout: 1 * time.Second})
 	if err!=nil{
 	log.Fatal(err)
 }
 defer db.Close()
 
 db.Update(func(tx *bolt.Tx) error {
-    b, err := tx.CreateBucketIfNotExists([]byte("bTech"))
+    b, err := tx.CreateBucketIfNotExists([]byte("AdServer"))
     if err != nil {
         return err
     }
-m, err:=tx.CreateBucketIfNotExists([]byte("MBA"))
-  if err != nil {
-	return err
-    }
-
-    b.Put([]byte("BioTech"), []byte("67"))
-    b.Put([]byte("cat"), []byte("lame"))
-    b.Put([]byte("liger"), []byte("awesome"))
+    b.Put([]byte("abc"), []byte("67"))
+    b.Put([]byte("abc1"), []byte("97.8"))
+    b.Put([]byte("abc2"), []byte("85.89"))
     c := b.Cursor()
     for k, v := c.First(); k != nil; k, v = c.Next() {
         fmt.Printf("A %s is %s.\n", k, v)
     }
-    m.Put([]byte("Finance"),[]byte("22"))
- m.Put([]byte("Marketing"),[]byte("222"))
- m.Put([]byte("Eco"),[]byte("224"))
-j:=m.Cursor()
-for t, y:=j.First();t!=nil;t,y =j.Next(){
-     fmt.Printf("IN %s number of students are %s.\n",t,y)
-}
-
-    return nil
+ return nil
 })
+
 db.Update(func(tx *bolt.Tx) error {
     b, err := tx.CreateBucketIfNotExists([]byte("bTech"))
     if err != nil {
         return err
     }
-  return b.Put([]byte("CSE"), []byte("68"))
-
-})
-db.Update(func(tx *bolt.Tx) error {
-    b, err := tx.CreateBucketIfNotExists([]byte("bTech"))
-    if err != nil {
-        return err
-    }
-  return b.Put([]byte("IT"), []byte("69"))
+  return b.Put([]byte("abc3"), []byte("68"))
 
 })
 err = db.View(func(tx *bolt.Tx) error {
-      j:=tx.Bucket([]byte("bTech"))
+      j:=tx.Bucket([]byte("subline_id"))
       c := j.Cursor()
     	for k, n := c.First(); k != nil; k, n = c.Next() {
         	fmt.Printf("key=%s, value=%s\n", k, n)
